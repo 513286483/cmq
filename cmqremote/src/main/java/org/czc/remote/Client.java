7773ddb5595b3071b2fa8997bb55573ac8e5bc07
chenzhichao <513286483@qq.com>
2019-04-03 01:00:16 +0800

    抄rocketmq的netty配置

--- cmqremote/src/main/java/org/czc/remote/Client.java
+++ cmqremote/src/main/java/org/czc/remote/Client.java
@@ -3,11 +3,10 @@ package org.czc.remote;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelConfig;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
 
 public class Client {
 
@@ -17,19 +16,31 @@ public class Client {
     public void init() {
         bootstrap = new Bootstrap();
         bootstrap.group(new NioEventLoopGroup())
-                .handler(new ChannelInitializer<Channel>() {
+                .option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.SO_KEEPALIVE, false)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_SNDBUF, 64 * 1024)
+                .option(ChannelOption.SO_RCVBUF, 64 * 1024)
+                .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(Channel channel) throws Exception {
-                        ChannelConfig channelConfig = channel.config();
-                        channelConfig.setConnectTimeoutMillis(5000)
-                                .setOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
-                        channelConfig.setOption(ChannelOption.SO_SNDBUF, 64 * 1024);
-                        channelConfig.setOption(ChannelOption.SO_RCVBUF, 64 * 1024);
-                        channelConfig.setOption(ChannelOption.SO_KEEPALIVE, false);
+                    protected void initChannel(SocketChannel ch) throws Exception {
                         channel.pipeline().addLast(new MsgCodec())
                                 .addLast(new NettyClientHandler());
                     }
                 });
+//                .handler(new ChannelInitializer<Channel>() {
+//                    @Override
+//                    protected void initChannel(Channel channel) throws Exception {
+//                        ChannelConfig channelConfig = channel.config();
+//                        channelConfig.setConnectTimeoutMillis(5000)
+//                                .setOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+//                        channelConfig.setOption(ChannelOption.SO_SNDBUF, 64 * 1024);
+//                        channelConfig.setOption(ChannelOption.SO_RCVBUF, 64 * 1024);
+//                        channelConfig.setOption(ChannelOption.SO_KEEPALIVE, false);
+//                        channel.pipeline().addLast(new MsgCodec())
+//                                .addLast(new NettyClientHandler());
+//                    }
+//                });
         channel = bootstrap.connect("127.0.0.1",9999).channel();
     }
 }
--- cmqremote/src/main/java/org/czc/remote/Server.java
+++ cmqremote/src/main/java/org/czc/remote/Server.java
@@ -8,29 +8,55 @@ import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.net.InetSocketAddress;
 
 public class Server {
 
     private ServerBootstrap bootstrap;
-    private Channel channel;
 
     public void init() {
+        boolean useEpoll = true;
         bootstrap = new ServerBootstrap();
-        bootstrap.group(new NioEventLoopGroup(1), new NioEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2));
-        bootstrap.handler(new ChannelInitializer<Channel>() {
+        bootstrap.group(new EpollEventLoopGroup(1), new EpollEventLoopGroup(Runtime.getRuntime().availableProcessors() * 2));
+        bootstrap.channel(useEpoll ? EpollServerSocketChannel.class : NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .option(ChannelOption.SO_REUSEADDR, true)
+                .option(ChannelOption.SO_KEEPALIVE, false)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.SO_SNDBUF, 64*1024)
+                .childOption(ChannelOption.SO_RCVBUF, 64*1024)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
-                    protected void initChannel(Channel channel) throws Exception {
-                        ChannelConfig channelConfig = channel.config();
-                        channelConfig.setConnectTimeoutMillis(5000)
-                                .setOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
-                        channelConfig.setOption(ChannelOption.SO_SNDBUF, 64 * 1024);
-                        channelConfig.setOption(ChannelOption.SO_RCVBUF, 64 * 1024);
-                        channelConfig.setOption(ChannelOption.SO_KEEPALIVE, false);
-                        channel.pipeline().addLast(new MsgCodec())
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new MsgCodec())
                                 .addLast(new NettyClientHandler());
                     }
                 });
+//        bootstrap.option(ChannelOption.TCP_NODELAY, true)
+//                .option(ChannelOption.SO_BACKLOG, 1024)
+//                .option(ChannelOption.SO_SNDBUF, 64 * 1024)
+//                .option(ChannelOption.SO_SNDBUF, 64 * 1024)
+//                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+//        bootstrap.handler(new ChannelInitializer<Channel>() {
+//                    @Override
+//                    protected void initChannel(Channel channel) throws Exception {
+//                        ChannelConfig channelConfig = channel.config();
+//                        channelConfig.setConnectTimeoutMillis(5000)
+//                                .setOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+//                        channelConfig.setOption(ChannelOption.SO_SNDBUF, 64 * 1024);
+//                        channelConfig.setOption(ChannelOption.SO_RCVBUF, 64 * 1024);
+//                        channelConfig.setOption(ChannelOption.SO_KEEPALIVE, false);
+//                        channel.pipeline().addLast(new MsgCodec())
+//                                .addLast(new NettyClientHandler());
+//                    }
+//                });
         bootstrap.bind(9999).syncUninterruptibly();
     }
 }
